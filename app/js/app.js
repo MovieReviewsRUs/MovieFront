ec6e2f006248fa698bc60d9a1508db9c6ec5ef5a
Helen Thi <ht5658@gmail.com>
2015-03-06 15:44:54 -0500

    added function to render all movies on page

--- app/js/app.js
+++ app/js/app.js
@@ -21,8 +21,34 @@ App.getMovies = function(){
     url: App.url + '/movies',
     type: 'GET',
   }).done(function(data){
-    trace(data);
+    App.render_all_movies(data);
   }).fail(function(jqXHR, textStatus, errorThrown){
     trace(jqXHR, textStatus, errorThrown);
   });
+
+  App.render_all_movies = function(movies){
+   var html = '';
+   html += "<ul>"
+    movies.forEach(function(movie){
+      html += '<article>'
+      html += "<h2>" + movie.title + "</h2>";
+      html += '<li> Total Gross: ' + movie.total_gross + "</li>";
+      html += '<li> MPAA Rating: ' + movie.mpaa_rating + "</li>";
+      html += '<li> Release Date: ' + movie.release_date + "</li>";
+      html += '<li> Description: ' + movie.description + "</li>";
+      if (movie.reviews.length > 0){
+        movie.reviews.forEach(function(review){
+        html += "<br><h3>Reviews: </h3>";
+        html += '<li> Author: ' + review.author + "</li>";
+        html += '<li> Comment: ' + review.comment + "</li>";
+        html += '<li> Rating(Number of Stars): ' + review.star_rating + "</li>";
+        html += "<ul>";
+        html += "<li>" + review.name + "</li>";
+        });
+      };
+       html += '<article>'
+       html += "</ul>";
+    });
+    $('.movies').append(html);
+  };
 };
