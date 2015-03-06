6070779e1ca5da19b9d3447c23f46c5cce4b9267
pat <patrick.h.needham@gmail.com>
2015-03-06 15:10:34 -0500

    ajax running in console for movie index

--- app/js/app.js
+++ app/js/app.js
@@ -7,8 +7,22 @@ var trace = function(){
   }
 };
 
-var App = App || {};
+var App = App || {
+  url: 'http://localhost:3000'
+};
 
 $(document).ready(function(){
   trace('hello world');
+  App.getMovies();
 });
+
+App.getMovies = function(){
+  $.ajax({
+    url: App.url + '/movies',
+    type: 'GET',
+  }).done(function(data){
+    trace(data);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  });
+};
